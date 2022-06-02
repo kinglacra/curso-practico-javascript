d06a4f64053a26d5d0d62852a1b79cf88b3ce2ed
KingLacra <diegobarrera2000@gmail.com>
2022-06-02 10:37:41 -0400

    primer commit, aun faltan archivos por terminar, clase 1 a la mitad

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # curso-practico-javascript
+
+## Taller 1: figuras geometricas
+
+- primer paso: definir las formulas
+- segundo paso: implementar las formulas en javascript
+- tercer paso: crear funciones
+- cuarto paso: integrar javascript con HTML 
---
+++ figuras.js
@@ -0,0 +1,16 @@
+//codigo del cuadrado
+const ladoCuadrado = 5;
+console.log("los lados del cuadrado miden: " + ladoCuadrado + "cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("el perimetro del cuadrado es: " + perimetroCuadrado + "cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("el área del cuadrado es: " + areaCuadrado + "cm2");
+
+//codigo del triangulo
+const ladotriangulo1 = 6;
+const ladotriangulo2 = 6;
+const ladotriangulo3 = 4;
+
+console.log("los lados del triangulo miden: " + ladotriangulo1 + "cm, " + ladotriangulo2 + "cm, " + ladotriangulo3 + "cm");
