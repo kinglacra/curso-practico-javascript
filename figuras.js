2ae739cf2b50cf0e8da5cb487058ad280f41889c
KingLacra <diegobarrera2000@gmail.com>
2022-06-08 09:26:15 -0400

    Clase de figuras geometricas terminada, falta el reto

--- figuras.js
+++ figuras.js
@@ -13,7 +13,7 @@ function areaCuadrado(lado){
 //codigo del triangulo
 
 function perimetroTriangulo(lado1, lado2, base){
-    return lado1 + lado2 + base;
+    return (Number(lado1) + Number(lado2) + Number(base));
 };
 
 
@@ -43,3 +43,75 @@ function perimetroCirculo(radio){
 function areaCirculo(radio){
     return (radio * radio) * PI;
 };
+
+//interactuando con HTML
+//Cuadrado
+
+function calcularPerimetroCuadrado(){
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
+
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+};
+
+function calcularAreaCuadrado(){
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
+
+    const area = areaCuadrado(value);
+    alert(area);
+};
+
+//Circulo
+
+function calcularDiametroCirculo(){
+    const input = document.getElementById("inputCirculo");
+    const value = input.value;
+
+    const diametro = diametroCirculo(value)
+    alert(diametro);
+};
+
+function calcularAreaCirculo(){
+    const input = document.getElementById("inputCirculo");
+    const value = input.value;
+
+    const area = areaCirculo(value);
+    alert(area);
+};
+
+function calcularPerimetroCirculo(){
+    const input = document.getElementById("inputCirculo");
+    const value = input.value;
+
+    const perimetro = perimetroCirculo(value);
+    alert(perimetro);
+}
+
+//Triangulo
+
+function calcularPerimetroTriangulo(){
+    const input1 = document.getElementById("inputLado1");
+    const input2 = document.getElementById("inputLado2");
+    const input3 = document.getElementById("inputBase");
+
+    const value1 = input1.value;
+    const value2 = input2.value;
+    const value3 = input3.value;
+
+    const perimetro = perimetroTriangulo(value1, value2, value3);
+    alert(perimetro);
+}
+
+function calcularAreaTriangulo(){
+    const input1 = document.getElementById("inputBase");
+    const input2 = document.getElementById("inputAltura");
+
+    
+    const value1 = input1.value;
+    const value2 = input2.value;
+
+    const area = areaTriangulo(value1, value2);
+    alert(area);
+}
